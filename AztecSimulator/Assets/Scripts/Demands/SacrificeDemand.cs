48d4e8aca0618f0b52b301274eb2425a72d426dc
brendan-lojam <33971709+brendan-lojam@users.noreply.github.com>
2019-04-27 19:26:06 -0400

    break up demands structure so we can get more specific demands

---
+++ AztecSimulator/Assets/Scripts/Demands/SacrificeDemand.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SacrificeDemand
+{
+	private static int sId = 0;
+
+	public int mId;
+	public SacrificeResult mSatisfiedResult;
+	public SacrificeResult mIgnoredResult;
+
+	public SacrificeDemand(SacrificeResult satisfiedResult, SacrificeResult ignoredResult) {
+
+		mId = ++sId;
+
+		mSatisfiedResult = satisfiedResult;
+		mIgnoredResult = ignoredResult;
+	}
+
+	public abstract bool CheckSatisfaction(List<Person> people);
+	public abstract string GetString();
+
+	public void DebugPrint()
+	{
+		Debug.Log(GetString());
+	}
+}
---
+++ AztecSimulator/Assets/Scripts/Demands/SimpleDemand.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleDemand : SacrificeDemand {
+
+	public List<Person.Attribute> mDemandedAttributes;
+
+	public SimpleDemand(SacrificeResult satisfiedResult, SacrificeResult ignoredResult) : base(satisfiedResult, ignoredResult)
+	{
+		int numAttributes = Random.Range(1,3);
+		mDemandedAttributes = GenerateSatisfiableDemands(numAttributes);		
+	}
+
+	public override string GetString()
+	{
+		string satisfiedString = mSatisfiedResult == null ? "<demanded>" : mSatisfiedResult.mName;
+		string costString = GetDemandedAttributes();
+		return satisfiedString + "\r\nDEMAND\r\n" + costString;
+	}
+
+	public string GetDemandedAttributes()
+	{
+		return Utilities.ConcatStrings(mDemandedAttributes.ConvertAll(
+			attr => System.Enum.GetName(typeof(Person.Attribute), (int)attr)
+		), true);
+	}
+
+	public override bool CheckSatisfaction(List<Person> people)
+	{
+		List<Person.Attribute> demandsCopy = new List<Person.Attribute>(mDemandedAttributes);
+		foreach(Person p in people)
+		{
+			foreach(Person.Attribute attr in p.Attributes)
+			{
+				foreach(Person.Attribute demandedAttr in demandsCopy)
+				{
+					if(attr == demandedAttr)
+					{
+						demandsCopy.Remove(demandedAttr);
+						break;
+					}
+				}
+			}
+		}
+
+		return(demandsCopy.Count == 0);
+	}
+
+	public static List<Person.Attribute> GenerateSatisfiableDemands(int preferredNum)
+	{
+		List<Person.Attribute> demands = new List<Person.Attribute>(); 
+
+		// naive (or clever?): if we just pick a person at random and an attribute at random,
+		// then we're guaranted to be able to satisfy it. The bonus is that 
+		// some other people might share that attribute, so we get player choice involved basically for free
+		List<Person> people = Utilities.GetPersonManager().People;
+
+		while(preferredNum-- > 0)
+		{
+			Person p = people[Random.Range(0, people.Count)];
+			Person.Attribute attribute = p.Attributes[Random.Range(0, p.Attributes.Length)];
+			if(!demands.Contains(attribute))
+				demands.Add(attribute);
+		}
+
+		return(demands);
+	}
+
+	public static List<Person.Attribute> GenerateRandomDemands(int preferredNum)
+	{
+		List<Person.Attribute> demands = new List<Person.Attribute>();
+		var numAttrs = System.Enum.GetValues(typeof(Person.Attribute)).Length-1;
+		int[] attributes = Utilities.RandomList(numAttrs, preferredNum);
+		for(int i = 0; i < preferredNum; ++i)
+		{
+			demands.Add((Person.Attribute)attributes[i]);
+		}
+		return(demands);
+	}
+
+}
