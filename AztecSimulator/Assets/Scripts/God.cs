c69b408702c80f4d86256617ba8471d409bb196c
brendan-lojam <33971709+brendan-lojam@users.noreply.github.com>
2019-04-27 18:36:52 -0400

    Add a simple function to generate demands that we know can be met

--- AztecSimulator/Assets/Scripts/God.cs
+++ AztecSimulator/Assets/Scripts/God.cs
@@ -17,15 +17,8 @@ public class God : MonoBehaviour {
 
 			mId = ++sId;
 
-			mDemandedAttributes = new List<Person.Attribute>();
 			int numAttributes = Random.Range(1,3);
-
-			var numAttrs = System.Enum.GetValues(typeof(Person.Attribute)).Length-1;
-			int[] attributes = Utilities.RandomList(numAttrs, numAttributes);
-			for(int i = 0; i < numAttributes; ++i)
-			{
-				mDemandedAttributes.Add((Person.Attribute)attributes[i]);
-			}
+			mDemandedAttributes = GenerateSatisfiableDemands(numAttributes);		
 
 			mSatisfiedResult = satisfiedResult;
 			mIgnoredResult = ignoredResult;
@@ -41,6 +34,38 @@ public class God : MonoBehaviour {
 		public void DebugPrint()
 		{
 			Debug.Log(GetString());
+		}
+
+		public static List<Person.Attribute> GenerateSatisfiableDemands(int preferredNum)
+		{
+			List<Person.Attribute> demands = new List<Person.Attribute>(); 
+
+			// naive (or clever?): if we just pick a person at random and an attribute at random,
+			// then we're guaranted to be able to satisfy it. The bonus is that 
+			// some other people might share that attribute, so we get player choice involved basically for free
+			List<Person> people = Utilities.GetPersonManager().People;
+
+			while(preferredNum-- > 0)
+			{
+				Person p = people[Random.Range(0, people.Count)];
+				Person.Attribute attribute = p.Attributes[Random.Range(0, p.Attributes.Length)];
+				if(!demands.Contains(attribute))
+					demands.Add(attribute);
+			}
+			
+			return(demands);
+		}
+
+		public static List<Person.Attribute> GenerateRandomDemands(int preferredNum)
+		{
+			List<Person.Attribute> demands = new List<Person.Attribute>();
+			var numAttrs = System.Enum.GetValues(typeof(Person.Attribute)).Length-1;
+			int[] attributes = Utilities.RandomList(numAttrs, preferredNum);
+			for(int i = 0; i < preferredNum; ++i)
+			{
+				demands.Add((Person.Attribute)attributes[i]);
+			}
+			return(demands);
 		}
 	}
 
