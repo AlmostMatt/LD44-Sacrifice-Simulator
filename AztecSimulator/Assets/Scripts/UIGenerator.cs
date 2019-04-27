ef1cd31404ede52c5bcac31f8c52df5bd6744610
Matthew Hyndman <myndman@gmail.com>
2019-04-26 22:43:41 -0700

    Sacrifice everyone

--- AztecSimulator/Assets/Scripts/UIGenerator.cs
+++ AztecSimulator/Assets/Scripts/UIGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 // Generates UIPerson objects. Can be merged into PersonManager later
 public class UIGenerator : MonoBehaviour {
@@ -16,7 +17,7 @@ public class UIGenerator : MonoBehaviour {
 		mUiPeoplePool = new List<GameObject>();
 	}
 
-	
+	// TODO: use people-changed-listener instead of update
 	void Update () {
 		List<Person> people = mPersonManager.People;
 		for(int i = 0; i < Mathf.Max(people.Count, mUiPeoplePool.Count); i++)
@@ -34,12 +35,29 @@ public class UIGenerator : MonoBehaviour {
 			RectTransform rt = uiPerson.GetComponent<RectTransform>();
 			rt.anchoredPosition = new Vector2(0f,0f+30f*(i-people.Count/2));
 			// Update visibility
-			uiPerson.transform.gameObject.SetActive(i <= people.Count);
+			uiPerson.transform.gameObject.SetActive(i < people.Count);
 		}
 	}
 
+	private List<Person> getSelectedPeople() {
+		List<Person> people = mPersonManager.People;
+		List<Person> result = new List<Person>();
+		for(int i = 0; i < Mathf.Min(people.Count, mUiPeoplePool.Count); i++)
+		{
+			GameObject uiPerson = mUiPeoplePool[i];
+			Toggle selectedToggle = GetComponentInChildren<Toggle>();
+			Debug.Log("checking if someone is selected");
+			if (true || selectedToggle.isOn) {
+				result.Add(people[i]);
+			}
+		}
+		return result;
+	}
+
 	public void OnSacrifice() {
-		// sacrifice the selected people
-		Debug.Log("SACRIFICE");
+		List<Person> selectedPeople = getSelectedPeople();
+		Debug.Log("Sacrificing " + selectedPeople.Count + " people.");
+		// TODO: sacrifice to a god instead of just removing.
+		mPersonManager.RemovePeople(selectedPeople);
 	}
 }
