59d8ac59d29b0673b6b300aef481c274c53b8ec3
Matthew Hyndman <myndman@gmail.com>
2019-04-26 21:28:52 -0700

    Create a minimal UIgenerator to instantiate UIPerson objects

---
+++ AztecSimulator/Assets/Scripts/UIGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Generates UIPerson objects. Can be merged into PersonManager later
+public class UIGenerator : MonoBehaviour {
+
+	public GameObject uiPersonObject;
+	public int numStartingPeople = 10;
+
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < numStartingPeople; ++i)
+		{
+			GameObject person = Instantiate(uiPersonObject);
+			person.transform.SetParent(transform);
+			RectTransform rt = person.GetComponent<RectTransform>();
+			rt.anchoredPosition = new Vector2(0f,0f+30f*(i-numStartingPeople/2));
+		}
+	}
+
+	
+	void Update () {
+		
+	}
+}
