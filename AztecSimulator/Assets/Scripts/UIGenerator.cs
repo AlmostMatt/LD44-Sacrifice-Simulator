14cdc308fcc71d419efce80b7c396321497bd9ae
Matthew Hyndman <myndman@gmail.com>
2019-04-26 21:58:04 -0700

    UIGenerator creates people based on PersonManager's people

--- AztecSimulator/Assets/Scripts/UIGenerator.cs
+++ AztecSimulator/Assets/Scripts/UIGenerator.cs
@@ -6,21 +6,35 @@ using UnityEngine;
 public class UIGenerator : MonoBehaviour {
 
 	public GameObject uiPersonObject;
-	public int numStartingPeople = 10;
+
+	public PersonManager mPersonManager;
+	private List<GameObject> mUiPeoplePool;
 
 	// Use this for initialization
 	void Start () {
-		for(int i = 0; i < numStartingPeople; ++i)
-		{
-			GameObject person = Instantiate(uiPersonObject);
-			person.transform.SetParent(transform);
-			RectTransform rt = person.GetComponent<RectTransform>();
-			rt.anchoredPosition = new Vector2(0f,0f+30f*(i-numStartingPeople/2));
-		}
+		mUiPeoplePool = new List<GameObject>();
+		// FindWithTag   mPersonManager = (PersonManager)FindObjectOfType(typeof(PersonManager));
 	}
 
 	
 	void Update () {
-		
+		List<Person> people = mPersonManager.People;
+		for(int i = 0; i < Mathf.Max(people.Count, mUiPeoplePool.Count); i++)
+		{
+			GameObject uiPerson;
+			// Spawn new UI person
+			if (i >= mUiPeoplePool.Count) {
+				uiPerson = Instantiate(uiPersonObject);
+				mUiPeoplePool.Add(uiPerson);
+				uiPerson.transform.SetParent(transform);
+			} else {
+				uiPerson = mUiPeoplePool[i];
+			}
+			// Update position
+			RectTransform rt = uiPerson.GetComponent<RectTransform>();
+			rt.anchoredPosition = new Vector2(0f,0f+30f*(i-people.Count/2));
+			// Update visibility
+			uiPerson.transform.gameObject.SetActive(i <= people.Count);
+		}
 	}
 }
