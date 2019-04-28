cb41a80a240c507aa9aa73d09d9748476e54724f
brendan-lojam <33971709+brendan-lojam@users.noreply.github.com>
2019-04-27 20:42:35 -0400

    Boons for xp gain

---
+++ AztecSimulator/Assets/Scripts/Boons/XpBuff.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XpBuff : SacrificeResult {
+
+	private Person.Attribute mProfession;
+
+	public XpBuff(Person.Attribute profession, string name, string description) : base(name, description)
+	{
+		mProfession = profession;
+	}
+
+	public override void DoEffect()
+	{
+		GameState.AddXpBuff(mProfession, 2);
+	}
+
+}
