ffded0a8a55797ebd52a72df6fb2779574e81027
brendan-lojam <33971709+brendan-lojam@users.noreply.github.com>
2019-05-14 22:21:57 -0400

    Rough in main menu
    - not sure why the splash screen background aspect changed

---
+++ AztecSimulator/Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour {
+
+	public string gameScene;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void NewGame()
+	{
+		Debug.Log("New game!");
+		SceneManager.LoadScene(gameScene);
+	}
+
+	public void Tutorial()
+	{
+		Debug.Log("Tutorial!");
+	}
+}
