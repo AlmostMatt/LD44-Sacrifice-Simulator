bf450ef9585353663b6db2bc36e3d14fb5f59dab
brendan-lojam <33971709+brendan-lojam@users.noreply.github.com>
2019-05-19 03:27:05 -0400

    some other changes. wrong description in my last changes...
    previous change added a dialog to the tutorial. not sure why the button doesn't work yet

---
+++ AztecSimulator/Assets/Scripts/TutorialScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialScript : MonoBehaviour, IDialogCallback {
+
+	private int mStage;
+
+	// Use this for initialization
+	void Start () {
+
+		mStage = 0;
+
+		/// Utilities.GetTimingManager().SetPaused(true);
+		Utilities.GetUIManager().ShowMessage("This is a game about sacrificing people", this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnDialogClosed()
+	{
+		++mStage;
+		if(mStage == 1)
+		{
+			Utilities.GetUIManager().ShowMessage("Start by trying to do something cool", this);
+		}
+	}
+}
