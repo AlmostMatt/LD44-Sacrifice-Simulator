31b16b12e7a511b7852fdf9ea7e29d15bb12b672
brendan-lojam <33971709+brendan-lojam@users.noreply.github.com>
2019-05-18 20:37:14 -0400

    make music a bit quieter

--- AztecSimulator/Assets/Scripts/TimingManager.cs
+++ AztecSimulator/Assets/Scripts/TimingManager.cs
@@ -5,7 +5,14 @@ using UnityEngine.UI;
 
 public class TimingManager : MonoBehaviour {
 
+	public bool startPaused = false;
+
 	private bool mPaused = false;
+
+	void Start()
+	{
+		SetPaused(startPaused);
+	}
 
 	// MUST be the first script to run each frame, so that the proper gameDeltaTime is set for all other systems
 	void Update () {
@@ -23,7 +30,12 @@ public class TimingManager : MonoBehaviour {
 	}
 
 	public void TogglePause() {
-		mPaused = !mPaused;
+		SetPaused(!mPaused);
+	}
+
+	private void SetPaused(bool paused)
+	{
+		mPaused = paused;
 		if(mPaused)
 			GameState.GameSpeed = 0;
 		else
