5c9bd1b173f1ea37fe67e62e60dd2ecce26aac74
Matthew Hyndman <myndman@gmail.com>
2019-05-21 22:25:37 -0700

    Position hover text above the relevant box instead of above the cursor. This way it will not cover the relevant box.

--- AztecSimulator/Assets/Scripts/UI/HoverInfo.cs
+++ AztecSimulator/Assets/Scripts/UI/HoverInfo.cs
@@ -62,25 +62,37 @@ public class HoverInfo :  MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
 	}
 
 	private void UpdatePosition() {
-		// HACK
-		// The size of this is 0,0 until it is enabled. So place it offscreen until it is enabled
-		// and once it has a size do the math to make it not go off the edge of the screen
+        // HACK
+        // The size of this is 0,0 until it is enabled. So place it offscreen until it is enabled
+        // and once it has a size do the math to make it not go off the edge of the screen
 		Rect uiTextRect = mUiInfo.GetComponent<RectTransform>().rect;
 		if (uiTextRect.width == 0) {
 			//Debug.Log("info text is temporarily offscreen");
 			mUiInfo.transform.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, mCanvasTransform.rect.height);
 			return;
 		}
-		Vector2 desiredPosition = new Vector2(
+        Rect mainRect = LocalRectToScreenRect(gameObject.GetComponent<RectTransform>());
+        Vector2 desiredPosition = new Vector2(mainRect.x + mainRect.width / 2, mainRect.y + mainRect.height + uiTextRect.height / 2);
+        if (desiredPosition.y + uiTextRect.height / 2 > mCanvasTransform.rect.height)
+        {
+            desiredPosition.y = mainRect.y - uiTextRect.height / 2;
+        } 
+        Vector2 actualPosition = new Vector2(
 			Mathf.Min(Mathf.Max(
-				mDesiredPosition.x, 
+                desiredPosition.x, 
 				uiTextRect.width/2),
 			mCanvasTransform.rect.width - (uiTextRect.width/2)),
 			Mathf.Min(Mathf.Max(
-				mDesiredPosition.y,
+                desiredPosition.y,
 				uiTextRect.height/2),
 				mCanvasTransform.rect.height- (uiTextRect.height/2)));
-		mUiInfo.transform.GetComponent<RectTransform>().anchoredPosition = desiredPosition - mCanvasTransform.anchoredPosition;
+		mUiInfo.transform.GetComponent<RectTransform>().anchoredPosition = actualPosition - mCanvasTransform.anchoredPosition;
 		// set global position relative to mouse
 	}
+
+    public static Rect LocalRectToScreenRect(RectTransform transform)
+    {
+        Vector2 worldSize = Vector2.Scale(transform.rect.size, transform.lossyScale);
+        return new Rect((Vector2)transform.position - (worldSize * 0.5f), worldSize);
+    }
 }
