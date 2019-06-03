25e1bd985d18b07d275ea359b2a44f222395cc61
Matthew Hyndman <myndman@gmail.com>
2019-06-02 22:44:16 -0700

    Add a 2s buffer before starvation causes damage.

--- AztecSimulator/Assets/Scripts/Person.cs
+++ AztecSimulator/Assets/Scripts/Person.cs
@@ -167,6 +167,7 @@ public class Person : MonoBehaviour, IRenderable {
 		get { return((int)Mathf.Floor(mAge)); }
 	}
 
+    private float mHungryBuffer=0f;
 	private bool mIsHungry;
 	public bool Hungry
 	{
@@ -265,15 +266,26 @@ public class Person : MonoBehaviour, IRenderable {
 		float healthDecayRate = mBaseHealthDecayRate; // 0.
 		if(mIsHungry)
 		{
-			// healthDecayRate *= 5;
-			healthDecayRate = 3f;
-		}
-		else if(GameState.HasBoon(BoonType.SURPLUS_FOOD_TO_HEALING))
-		{
-			float healAmount = GameState.GetBoonValue(BoonType.SURPLUS_FOOD_TO_HEALING) / 100f;
-			healthDecayRate -=  healAmount * GameState.FoodSurplus;
-		}
-		Damage(healthDecayRate * GameState.GameDeltaTime);
+            if (mHungryBuffer > 0f)
+            {
+                mHungryBuffer -= GameState.GameDeltaTime;
+            }
+            else
+            {
+                healthDecayRate = 3f;
+            }
+        }
+        else
+        {
+            // Allow 2 seconds before starvation will cause damage.
+            mHungryBuffer = 2f;
+            if(GameState.HasBoon(BoonType.SURPLUS_FOOD_TO_HEALING))
+		    {
+			    float healAmount = GameState.GetBoonValue(BoonType.SURPLUS_FOOD_TO_HEALING) / 100f;
+			    healthDecayRate -=  healAmount * GameState.FoodSurplus;
+		    }
+        }
+        Damage(healthDecayRate * GameState.GameDeltaTime);
 		// Time since health changed is strictly for UI purposes, so it does not use game-time.
 		mTimeSinceHealthChanged += Time.deltaTime;
 		if (mTimeSinceHealthChanged >= 1f) {
