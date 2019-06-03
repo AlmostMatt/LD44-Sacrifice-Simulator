ec0a102fba32b88b5a8022026fd0e4968b71190b
Matthew Hyndman <myndman@gmail.com>
2019-06-02 23:19:10 -0700

    increase the level of detail logged by the EditedObject finder

---
+++ AztecSimulator/Assets/Editor/EditedObjectFinder.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+public class EditedObjectFinder {
+    // Workaround taken from 
+    // https://issuetracker.unity3d.com/issues/scene-is-instantly-marked-dirty-when-opened-if-any-object-is-affected-by-both-a-contentsizefitter-and-verticallayoutgroup
+
+    static EditedObjectFinder()
+    {
+        Undo.postprocessModifications += OnPostProcessModifications;
+    }
+
+    private static UndoPropertyModification[] OnPostProcessModifications(UndoPropertyModification[] propertyModifications)
+    {
+        // I only care about the edits that happen when I stop playing.
+        if (EditorApplication.isPlaying)
+        {
+            return propertyModifications;
+        }
+        Debug.LogWarning(string.Format("EditedObjectFinder:  {0} objects were edited automatically.", propertyModifications.Length));
+        for (int i = 0; i < propertyModifications.Length; i++)
+        {
+            string propertyName = propertyModifications[i].currentValue.propertyPath;
+            string oldValue = propertyModifications[i].previousValue.value;
+            string newValue = propertyModifications[i].currentValue.value;
+            // propertyModifications[i].currentValue.target.GetType().ToString();
+            Transform obj = (Transform)propertyModifications[i].currentValue.target;
+            string objectName = obj.name;
+            while (obj.parent != null)
+            {
+                obj = obj.parent;
+                objectName = obj.name + "/" + objectName;
+            }
+            Debug.LogWarning(string.Format("{0} : {1} = {2} -> {3}", objectName, propertyName, oldValue, newValue));
+            // TODO: either undo the change, or disable/enable the relevant ScrollRect component
+        }
+        return propertyModifications;
+    }
+} 
